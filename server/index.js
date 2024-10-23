bc24974801bd87da883dd11b9738fe7e034a5f62
dhinaashwin <dhinaashwin11@gmail.com>
2024-10-23 17:55:00 +0530

    new

--- server/index.js
+++ server/index.js
@@ -1,44 +1,51 @@
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors({
-  origin: 'https://kiranrup.vercel.app',
-  credentials: true
-}));
+app.use(cors());
 app.use(bodyParser.json());
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', 'https://kiranrup.vercel.app')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}  
 
 // Transporter for Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'gmail', // You can use another email service like Outlook, Yahoo, etc.
   auth: {
-    user: 'dhinaashwin11@gmail.com',
-    pass: 'zdsonclqrqpzeezi',
+    user: 'dhinaashwin11@gmail.com', // Your Gmail account
+    pass: 'zdsonclqrqpzeezi', // App-specific password or your account password
   },
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log('Error in nodemailer configuration: ', error);
-  } else {
-    console.log('Nodemailer is ready to send emails');
-  }
-});
-app.get('/',(req,res) => {
-    res.send('Connected');
-}) 
-
 app.post('/send-email', (req, res) => {
-  console.log(req.body); // Log incoming request data
   const { name, email, mobile, message } = req.body;
 
   const mailOptions = {
     from: email,
-    to: 'kiranrup05@gmail.com',
-    subject: `New message from ${name}`,
-    text: `You have a new message from:
+    to: 'kiranrup05@gmail.com', // The recipient's email address
+    subject: `New message from ${name},
+    text: You have a new message from:
            Name: ${name}
            Email: ${email}
            Mobile: ${mobile}
@@ -55,8 +62,7 @@ app.post('/send-email', (req, res) => {
     }
   });
 });
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+}); 
